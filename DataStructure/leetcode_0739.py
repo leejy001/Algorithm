ad41fc5154cde191ab06d36ff79c740515736b15
leejy001 <greatness007@naver.com>
2021-03-03 20:53:47 +0900

    Daily Temperatures(Stack/Queue) /leetcode_0739/medium/python

---
+++ DataStructure/leetcode_0739.py
@@ -0,0 +1,14 @@
+from typing import List
+
+# 스택을 이용한 풀이
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        dist = [0]
+        s = [0]*len(T)
+        for i in range(1, len(T)):
+            while dist and T[i] > T[dist[-1]]:
+                last = dist.pop()
+                s[last] = (i - last)
+            else:
+                dist.append(i)
+        return s
