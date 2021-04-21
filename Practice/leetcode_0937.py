3eef30c3f8eaff96e56b9e8026c28c4c96888769
leejy001 <greatness007@naver.com>
2021-04-21 13:55:16 +0900

    Reorder Log Files /leetcode_0937/easy/python

---
+++ Practice/leetcode_0937.py
@@ -0,0 +1,14 @@
+from typing import List
+
+# lambda 이용 28ms
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        
+        letters.sort(key=lambda x : (x.split()[1:], x.split()[0]))
+        return letters + digits
