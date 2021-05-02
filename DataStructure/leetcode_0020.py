3d36a164df787646ee7b420e0439c6c63c155385
leejy001 <greatness007@naver.com>
2021-05-02 13:53:30 +0900

    Valid Parentheses(Stack/Queue) /leetcode_0020/easy/python

--- DataStructure/leetcode_0020.py
+++ DataStructure/leetcode_0020.py
@@ -1,6 +1,27 @@
-# 스택 일치 여부 판별
+# 일반적인 풀이 44ms
+class Solution_one:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i in '({[':
+                stack.append(i)
+            elif i in ')}]':
+                if len(stack) == 0:
+                    return False
+                elif i == ')' and stack.pop() == '(':
+                    pass
+                elif i == ']' and stack.pop() == '[':
+                    pass
+                elif i == '}' and stack.pop() == '{':
+                    pass
+                else:
+                    return False
+        if len(stack) != 0:
+            return False
+        return True
 
-class Solution:
+# 스택 일치 여부 판별 28ms
+class Solution_two:
     def isValid(self, s: str) -> bool:
         di = {')':'(', '}':'{', ']':'['}
         st = []
@@ -10,7 +31,5 @@ class Solution:
                     return False
             else:
                 st.append(s[i])
-        if st:
-            return False
-        else:
-            return True
+
+        return len(st) == 0
