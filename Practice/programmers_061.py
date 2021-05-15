d9b7dcc84d5b7fd4b13fe839524ab624615b5531
leejy001 <greatness007@naver.com>
2021-05-15 15:16:33 +0900

    2개 이하로 다른 비트 /programmers/LV2/python

--- Practice/programmers_061.py
+++ Practice/programmers_061.py
@@ -3,6 +3,7 @@ def solution(numbers):
     for number in numbers:
         n = 0
         while True:
+            # 0111 ^ 0111 + 0001, 0010, 0100, 1000
             if bin(number ^ number + 2**n).count('1') <= 2:
                 answer.append(number + 2**n)
                 break
