189c1e945003b9fad2ef8088aeceb84a8d842fba
leejy001 <greatness007@naver.com>
2021-06-11 13:16:32 +0900

    땅따먹기 /programmers/LV.2/python

---
+++ DP/programmers_097.py
@@ -0,0 +1,9 @@
+def solution(land):
+    row = len(land)
+    for i in range(1, row):
+        land[i][0] += max(land[i-1][1], land[i-1][2], land[i-1][3])
+        land[i][1] += max(land[i-1][0], land[i-1][2], land[i-1][3])
+        land[i][2] += max(land[i-1][0], land[i-1][1], land[i-1][3])
+        land[i][3] += max(land[i-1][0], land[i-1][1], land[i-1][2])
+        
+    return max(land[row-1])
