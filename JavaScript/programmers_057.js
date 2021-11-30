79d0cdd1a6f3c1622b1b97016a159c9c58e57082
leejy001 <greatness007@naver.com>
2021-11-30 22:18:52 +0900

    124 나라의 숫자/LV.2/연습

---
+++ JavaScript/programmers_057.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = '';
+    var num = [1, 2, 4];
+    while(n > 0) {
+        n -= 1
+        answer = num[n % 3] + answer
+        n = parseInt(n / 3)
+    }
+    return answer;
+}
