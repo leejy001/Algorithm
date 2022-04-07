bc9c57079b0c143981df0e9eb491ad827c4b3768
leejy001 <greatness007@naver.com>
2022-04-07 16:15:12 +0900

    예상 대진표/LV.2/연습

---
+++ JavaScript/programmers_074.js
@@ -0,0 +1,11 @@
+// LV.2 예상 대진표
+
+function solution(n, a, b) {
+  var answer = 0;
+  while (a != b) {
+    a = Math.floor((a + 1) / 2);
+    b = Math.floor((b + 1) / 2);
+    answer++;
+  }
+  return answer;
+}
