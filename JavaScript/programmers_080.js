68a82cb227d2517633d975dbd28b2aec0eb5c990
leejy001 <greatness007@naver.com>
2022-04-11 14:23:49 +0900

    H-Index/LV.2/연습

---
+++ JavaScript/programmers_080.js
@@ -0,0 +1,10 @@
+// LV.2 H-Index
+
+function solution(citations) {
+  var answer = 0;
+  citations.sort((a, b) => b - a);
+  for (let i = 0; i < citations.length; i++) {
+    if (i < citations[i]) answer++;
+  }
+  return answer;
+}
