402a9a040ffac27809a0d2250fa5c82edfbe8d48
leejy001 <greatness007@naver.com>
2023-03-09 10:52:05 +0900

    귤 고르기/Lv.2/연습

---
+++ JavaScript/programmers_167.js
@@ -0,0 +1,19 @@
+// LV.2 귤 고르기
+
+function solution(k, tangerine) {
+  const obj = {};
+  tangerine.forEach((n) => {
+    obj[n] = ++obj[n] || 1;
+  });
+  const kind = Object.values(obj).sort((a, b) => b - a);
+
+  let sum = 0;
+  let answer = 0;
+
+  for (let num of kind) {
+    answer++;
+    sum += num;
+    if (sum >= k) break;
+  }
+  return answer;
+}
